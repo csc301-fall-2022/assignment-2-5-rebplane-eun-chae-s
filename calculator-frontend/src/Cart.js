e39591518b39eb1d7950c190c0ce7ba80ed94d3f
eun-chae-s <s9710173@naver.com>
2022-10-11 20:06:29 -0400

    put api calls in frontend

--- calculator-frontend/src/Cart.js
+++ calculator-frontend/src/Cart.js
@@ -1,51 +1,71 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
 import Header from "./components/Header";
 import CreateItem from "./components/CreateItem";
 import Calculate from "./components/Calculate";
 import ItemTableHead from "./components/ItemTableHead";
 import Item from "./components/Item";
+import axios from "axios";
 
 function Cart() {
+  const [items, setItems] = useState([]);
 
-  const [items, setItems] = useState([])
+  useEffect(() => {
+    axios
+      .get("/api/items/")
+      .then((res) => setItems(res.data))
+      .catch((err) => console.log(err));
+  }, []);
 
   function addItem(item) {
-      setItems(prevItems => {
-        return [...prevItems, item];
-      });
+    if (items.length === 0) {
+      item.id = 1;
+    } else {
+      item.id = items[items.length - 1].id + 1;
+    }
+
+    // axios.put: for editing new one
+    axios.post("/api/items/", item).then((res) => {
+      axios
+        .get("/api/items/")
+        .then((res) => setItems(res.data))
+        .catch((err) => console.log(err));
+    });
   }
 
   function deleteItem(id) {
-      setItems(prevItems => {
-        return prevItems.filter((item, index) => {
-          return index !== id
-        });
-      });
+    axios.delete(`/api/items/${id}`).then((res) => {
+      axios
+        .get("/api/items/")
+        .then((res) => setItems(res.data))
+        .catch((err) => console.log(err));
+    });
   }
 
   return (
     <div>
       <Header />
-      <ItemTableHead/>
+      <ItemTableHead />
       {/* Maps the items in the array items to the table to be displayed*/}
       <table>
         <tbody>
-        {/* Send the mapped values to map to Item rows*/}
-        {items.map((item, index) => {
-          return (<Item
-              key={index}
-              id={index}
-              name={item.name}
-              quantity={item.quantity}
-              price={item.price}
-              deleteItem={deleteItem}/>
-          )
-        })}
+          {/* Send the mapped values to map to Item rows*/}
+          {items.map((item, index) => {
+            return (
+              <Item
+                key={index}
+                id={item.id}
+                name={item.name}
+                quantity={item.quantity}
+                price={item.price}
+                deleteItem={deleteItem}
+              />
+            );
+          })}
         </tbody>
       </table>
 
       <CreateItem addItem={addItem} />
-      <Calculate/>
+      <Calculate />
     </div>
   );
 }
