e7adf4dcd72853fd4208664dc7da70c41c9e8121
rebplane <rebecca_nhan@rogers.com>
2022-10-13 20:26:24 -0400

    Added template to urls.py to link to static files

--- calculator_backend/urls.py
+++ calculator_backend/urls.py
@@ -14,13 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
 # from rest_framework import routers
 # from calculator import views
 # from calculator_backend import calculator
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path('.*', TemplateView.as_view(template_name="index.html"))
 ]
 
 # forward requests with the pattern "/" to the module "calculator.urls"
