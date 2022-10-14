fd801d4fdd6bbe9aa33bb9722defc18f5af43bef
rebplane <rebecca_nhan@rogers.com>
2022-10-13 20:48:22 -0400

    Added build file

--- calculator_backend/urls.py
+++ calculator_backend/urls.py
@@ -22,7 +22,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path('.*', TemplateView.as_view(template_name="index.html"))
+
 ]
 
 # forward requests with the pattern "/" to the module "calculator.urls"
